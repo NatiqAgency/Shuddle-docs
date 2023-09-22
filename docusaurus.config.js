2c6ecab7e315eefe9585031b1b772e473e6c0dfa
VianneyDev <vianneytraina@gmail.com>
2023-09-22 11:11:04 +0200

    set docs to homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,10 +39,7 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          routeBasePath: '/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -67,7 +64,7 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Docs',
           },
         ],
       },
